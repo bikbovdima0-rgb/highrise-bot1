0fe9c9a7724550b9b54b657ba6f5e87a2a5e4b42
bikbovdima0-rgb <bikbovdima0@gmail.com>
2026-04-05 00:49:19 +0500

    Remove user join and leave messages

--- bot.py
+++ bot.py
@@ -7,9 +7,3 @@ class MyBot(BaseBot):
     async def on_chat(self, user, message: str) -> None:
         if message == "/hello":
             await self.highrise.chat(f"Привет, {user.username}!")
-
-    async def on_user_join(self, user) -> None:
-        await self.highrise.chat(f"👋 Добро пожаловать, {user.username}!")
-
-    async def on_user_leave(self, user) -> None:
-        await self.highrise.chat(f"👋 Пока, {user.username}!")
